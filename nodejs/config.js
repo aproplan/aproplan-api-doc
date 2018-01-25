5285fab8be27bce82b68f505a60dce78afd4c028
Sergio Ristagno <35029530+sristagno@users.noreply.github.com>
2018-01-25 07:38:36 +0100

    Delete config.js

--- nodejs/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    apiUrl: "https://api-tst.aproplan.com",
-    requesterId: "6F8C30D3-DB6C-4E8A-A190-7AD52B325936",
-    apiVersion: 7,
-    login: "sergio.ristagno@aproplan.com",
-    password: "aproplan"
-}
-
-module.exports = config;
